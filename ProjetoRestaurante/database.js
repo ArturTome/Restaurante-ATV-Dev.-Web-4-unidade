36986029f252606d67c6965a2da88224cb10f56c
Artur Tome <arturtomex@gmail.com>
2025-11-11 18:14:13 -0300

    Refactor database.js for better readability

--- ProjetoRestaurante/database.js
+++ ProjetoRestaurante/database.js
@@ -1,6 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./banco.db');
 
+// Tabelas do SQLite
+
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS clientes (
@@ -34,3 +36,4 @@ db.serialize(() => {
 });
 
 module.exports = db;
+
